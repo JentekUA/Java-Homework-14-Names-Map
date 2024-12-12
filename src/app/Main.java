7c12814c9e4ba02149f82f5c2afb70e35a9d93db
oleksandr.aleksashyn <oleksandr.aleksashyn@gmail.com>
2024-12-12 18:07:21 +0200

    Add requested functionality to Main class

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,12 @@
 package app;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        DataHandler handler = new DataHandler();
+        UiOperator uiOperator = new UiOperator();
+        uiOperator.getOutput(handler.getAll());
+        uiOperator.getOutput(handler.getById(172));
     }
 }
+
